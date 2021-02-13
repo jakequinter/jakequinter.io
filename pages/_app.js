bf440e9a0f44767f377ad48af74216613dd8d9f1
jakequinter <500132870@fvtc.edu>
2021-02-13 16:26:12 -0500

    outsourced analytics to utils

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,12 @@
-import { useEffect } from 'react';
 import { DefaultSeo } from 'next-seo';
-import { useRouter } from 'next/router';
-import * as Fathom from 'fathom-client';
 
 import { AuthProvider } from '@/lib/auth';
+import { useAnalytics } from '@/utils/analytics';
 import SEO from '../next-seo.config';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID, {
-      includedDomains: ['https://jakequinter.io']
-    });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, []);
+  useAnalytics();
 
   return (
     <AuthProvider>
