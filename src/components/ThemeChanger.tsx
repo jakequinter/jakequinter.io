62d900da095cd6ecd10d57241841d5b329962270
jakequinter <hello@jakequinter.io>
2021-12-13 17:30:09 -0500

    toggle theme

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -21,7 +21,7 @@ export default function ThemeToggle() {
 
   return (
     <Switch
-      checked={theme === 'light'}
+      checked={theme === 'dark'}
       onChange={handleTheme}
       className={
         'relative bg-gray-400 dark:bg-darkgray-400 inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-0'
@@ -30,13 +30,13 @@ export default function ThemeToggle() {
       <span className="sr-only">Change light/dark mode</span>
       <span
         className={`${
-          theme === 'light' ? 'translate-x-5' : 'translate-x-0'
+          theme === 'dark' ? 'translate-x-5' : 'translate-x-0'
         } pointer-events-none relative inline-block h-5 w-5 rounded-full shadow transform ring-0 transition ease-in-out duration-500`}
       >
         <span
           className={`
             ${
-              theme === 'light'
+              theme === 'dark'
                 ? 'opacity-0 ease-out duration-100'
                 : 'opacity-100 ease-in duration-200'
             }
@@ -44,18 +44,18 @@ export default function ThemeToggle() {
           `}
           aria-hidden="true"
         >
-          <SunIcon className="text-[#FFEF5C]" />
+          <MoonIcon className="text-[#2EC8EE]" />
         </span>
 
         <span
           className={`${
-            theme === 'light'
+            theme === 'dark'
               ? 'opacity-100 ease-in duration-200'
               : 'opacity-0 ease-out duration-100'
           } absolute bg-gray-900 border border-gray-900 rounded-full inset-0 h-full w-full flex items-center justify-center transition-opacity`}
           aria-hidden="true"
         >
-          <MoonIcon className="text-[#2EC8EE]" />
+          <SunIcon className="text-[#FFEF5C]" />
         </span>
       </span>
     </Switch>
