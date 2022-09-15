b80ba46e04bd3b7196734393e9b9999dcda11d72
Jake Quinter <hello@jakequinter.io>
2022-09-15 16:58:27 -0400

    add prism-react-renderer back

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -34,13 +34,11 @@ const components = {
       code={props.children.props.children}
       language="jsx"
     >
-      {/* @ts-expect-error */}
       {({ style, tokens, getLineProps, getTokenProps }) => (
         <pre
           className="text-javascript text-left p-2 overflow-auto text-xs rounded-md"
           style={style}
         >
-          {/* @ts-expect-error */}
           {tokens.map((line, i) => (
             <div
               // @ts-ignore
@@ -49,7 +47,6 @@ const components = {
               {...getLineProps({ line, key: i })}
             >
               <span className="table-row">
-                {/* @ts-expect-error */}
                 {line.map((token, key) => (
                   <span key={key} {...getTokenProps({ token, key })} />
                 ))}
