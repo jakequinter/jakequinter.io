061a60a728b213f0d82a1513483bcd938feb609d
Jake Quinter <hello@jakequinter.io>
2022-12-20 15:58:50 -0500

    next 13 upgrades

--- app/bookshelf/BookGroup.tsx
+++ app/bookshelf/BookGroup.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { CaretRight } from 'phosphor-react';
 import { motion } from 'framer-motion';
@@ -19,17 +18,10 @@ export default function BookGroup({ books }: Props) {
         <div key={book.guid}>
           <Link href={book.link} passHref>
             <motion.div
-              className="flex items-center  rounded-lg bg-white shadow-md hover:cursor-pointer dark:bg-black"
+              className="flex items-center justify-between rounded-lg bg-white p-4 shadow-md hover:cursor-pointer dark:bg-black"
               whileHover={{ scale: 1.01 }}
             >
-              <Image
-                className="mr-4 rounded-l-lg"
-                src={book.enclosure?.url ?? '/images/book-placeholder.jpg'}
-                alt={`${book.title} cover`}
-                height={60}
-                width={60}
-              />
-              <div className="flex w-full flex-col justify-between truncate py-3 pr-8">
+              <div className="flex flex-col justify-between truncate pr-8">
                 <p className="truncate pr-8 text-gray-900 dark:text-gray-50">
                   {book.title}
                 </p>
@@ -38,8 +30,7 @@ export default function BookGroup({ books }: Props) {
                   {book.creator}
                 </p>
               </div>
-
-              <CaretRight className="mr-3 text-gray-500 dark:text-gray-800" />
+              <CaretRight className="text-gray-500 dark:text-gray-800" />
             </motion.div>
           </Link>
         </div>
