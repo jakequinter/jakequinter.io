366344f063da3f3be93fa989b4156820f3dbd448
Jake Quinter <hello@jakequinter.io>
2023-04-07 08:38:55 -0400

    Static generation for blogs

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,5 @@
+import { notFound } from 'next/navigation';
 import { format } from 'date-fns';
-
 import { serialize } from 'next-mdx-remote/serialize';
 import fs from 'fs';
 import matter from 'gray-matter';
@@ -14,36 +14,46 @@ export async function generateStaticParams() {
     // Remove file extensions for page paths
     .map(path => path.replace(/\.mdx?$/, ''))
     // Map the path into the static paths object required by Next.js
-    .map(slug => ({ params: { slug } }));
+    .map(slug => slug);
 
-  return paths;
+  return paths.map(slug => {
+    slug;
+  });
 }
 
 async function getBlogPost(slug: string) {
   const postFilePath = path.join(POSTS_PATH, `${slug}.mdx`);
-  const source = fs.readFileSync(postFilePath);
+  try {
+    const source = fs.readFileSync(postFilePath);
+    const { content, data } = matter(source);
 
-  const { content, data } = matter(source);
+    const mdxSource = await serialize(content, {
+      mdxOptions: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+      },
+      scope: data,
+    });
 
-  const mdxSource = await serialize(content, {
-    // Optionally pass remark/rehype plugins
-    mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [],
-    },
-    scope: data,
-  });
-
-  return {
-    source: mdxSource,
-    frontMatter: data,
-    readingTime: readingTime(content),
-  };
+    return {
+      source: mdxSource,
+      frontMatter: data,
+      readingTime: readingTime(content),
+    };
+  } catch (error) {
+    return null;
+  }
 }
 
 export default async function Post({ params }: { params: { slug: string } }) {
   const { slug } = params;
-  const { source, frontMatter, readingTime } = await getBlogPost(slug);
+  const post = await getBlogPost(slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  const { source, frontMatter, readingTime } = post;
 
   const date = new Date(frontMatter.publishedAt);
   const formattedDate = new Date(
