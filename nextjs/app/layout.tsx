274306a4f51e6f6ccf3fb5d9b2505ab313a3982a
Jake Quinter <hello@jakequinter.io>
2023-11-16 19:57:47 -0500

    ga

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,9 +1,9 @@
-import Script from 'next/script';
 import type { Metadata } from 'next';
 import { Montserrat } from "next/font/google";
 
 import classNames from '@/utils/classNames';
 import Footer from '@/components/Footer';
+import GA from '@/components/GA';
 import Nav from '@/components/Nav';
 import './globals.css';
 
@@ -38,22 +38,8 @@ export default function RootLayout({
         'bg-gray-100 text-base text-gray-700'
       )}
     >
-      <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
-        </Script>
-      </head>
       <body>
+        <GA />
         <Nav />
         <main className="mx-auto mt-40 max-w-screen-sm px-4 sm:px-0">
           <div className="mb-20" style={{ minHeight: 'calc(100vh - 289px)' }}>
