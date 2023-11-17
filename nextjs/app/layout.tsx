2503cd4fc644d5285c9ad996c2c8c9365e79128b
Jake Quinter <hello@jakequinter.io>
2023-11-16 19:14:17 -0500

    ga

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import type { Metadata } from 'next';
 import { Montserrat } from "next/font/google";
 
@@ -37,6 +38,21 @@ export default function RootLayout({
         'bg-gray-100 text-base text-gray-700'
       )}
     >
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+        </Script>
+      </head>
       <body>
         <Nav />
         <main className="mx-auto mt-40 max-w-screen-sm px-4 sm:px-0">
